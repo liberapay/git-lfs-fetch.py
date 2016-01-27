25a81117c22b0a85c6cfd9da53cc81bb5bb93243
Changaco <changaco@changaco.oy.lc>
2016-01-27 14:00:05 +0100

    modify --verbose argument to work with python 3.5.1

--- git_lfs/__main__.py
+++ git_lfs/__main__.py
@@ -8,7 +8,7 @@ p = argparse.ArgumentParser()
 p.add_argument('git_repo', nargs='?', default='.',
                help="if it's bare you need to provide a checkout_dir")
 p.add_argument('checkout_dir', nargs='?')
-p.add_argument('-v', '--verbose', action='count')
+p.add_argument('-v', '--verbose', action='count', default=0)
 args = p.parse_args()
 
 fetch(args.git_repo, args.checkout_dir, args.verbose)
